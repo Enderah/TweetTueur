c8ab20a8a3504860c8aa569f9d8ff5b9c7aad4fc
Enderah <langnanou@hotmail.fr>
2016-04-10 18:31:15 +0200

    ajouts des fichiers pixi a l'index.html et au path du serveur

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-var http = require("http");
-var url = require('url');
-var fs = require('fs');
-var hashtag = "";
+var http = require("http")
+var url = require('url')
+var fs = require('fs')
+var hashtag = ""
 
 
 fs.readFile(__dirname  + "/server.conf", function(error, data) {
   if (error) 
-    console.log("pas de fichier de configuration");
+    console.log("pas de fichier de configuration")
   else {
-    hashtag = data.toString();
-    console.log(hashtag);
+    hashtag = data.toString()
+    console.log(hashtag)
   }
 })
 
@@ -17,47 +17,51 @@ fs.readFile(__dirname  + "/server.conf", function(error, data) {
  * creation du serveur
  */
 var server = http.createServer(function(request, response) {
-  var path = url.parse(request.url).pathname;
+  var path = url.parse(request.url).pathname
 
   /*
    * function d'erreur en cas de path non valide
    */
   function error404() {
-    response.writeHead(404);
-    response.write("404 page not found.");
-    response.end();
+    response.writeHead(404)
+    response.write("404 page not found.")
+    response.end()
   }
 
 
   function sendResponse(responsePath, responseType, fail) {
     if (fail) {
-      error404();
-      return;
+      error404()
+      return
     }
     fs.readFile(__dirname + responsePath, function (error, data) {
       if (error)
-        error404();
+        error404()
       else {
-        response.writeHead(200, {"Content-Type": responseType});
-        response.write(data, "utf8");
-        response.end();
+        response.writeHead(200, {"Content-Type": responseType})
+        response.write(data, "utf8")
+        response.end()
       }
     })
   }
 
 
   if (path == '/')
-    sendResponse(path + "client/index.html", "text/html", false);
+    sendResponse(path + "client/index.html", "text/html", false)
   else if (path == "/client/js/socket.io.js")
-    sendResponse(path, "application/javascript", false);
+    sendResponse(path, "application/javascript", false)
+  else if (path == "/client/js/pixi.min.js")
+    sendResponse(path, "application/javascript", false)
+  else if (path == "/client/js/pixi.min.js.map")
+    sendResponse(path, "application/javascript", false)
   else if (path == "/client/js/index.js")
-    sendResponse(path, "application/javascript", false);
+    sendResponse(path, "application/javascript", false)
   else
-    sendResponse("", "", true);
-});
+    sendResponse("", "", true)
+})
 
 
-server.listen(1337);
+server.listen(1337)
 
 
 /*
@@ -68,7 +72,7 @@ server.listen(1337);
 var Twitter = require('node-tweet-stream')
 fs.readFile(__dirname  + "/config", function(error, data) {
   if (error)
-    console.log("pas de fichier de configuration");
+    console.log("pas de fichier de configuration")
   else {
     try {
       var t = new Twitter(JSON.parse(data.toString()))
@@ -78,14 +82,14 @@ fs.readFile(__dirname  + "/config", function(error, data) {
       })
       t.track(hashtag)
 
-      var io = require('socket.io')(server);
+      var io = require('socket.io')(server)
 
       io.on('connection', function (socket) {
 
         t.on('tweet', function (tweet) {
           socket.emit('tweet', tweet)
         })
-      });
+      })
     }
     catch (e)
     {
